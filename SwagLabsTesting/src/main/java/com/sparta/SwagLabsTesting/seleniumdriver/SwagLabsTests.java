ec1cfb9433329dbefd82d8604447e3bcb4fee4d6
Alena Krasheninnikova <alenakrash@mail.ru>
2022-11-10 17:34:30 +0300

    Packages for testing

---
+++ SwagLabsTesting/src/main/java/com/sparta/SwagLabsTesting/seleniumdriver/SwagLabsTests.java
@@ -0,0 +1,26 @@
+package com.sparta.SwagLabsTesting.seleniumdriver;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverLogLevel;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class SwagLabsTests {
+        private static WebDriver webDriver;
+
+        @BeforeAll
+        public static void setupAll() {
+            System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.setLogLevel(ChromeDriverLogLevel.SEVERE);
+            webDriver = new ChromeDriver(chromeOptions);
+        }
+    @BeforeEach
+    public void setup() {
+        webDriver.manage().deleteAllCookies();
+        webDriver.get("https://news.ycombinator.com/");
+    }
+
+    }
