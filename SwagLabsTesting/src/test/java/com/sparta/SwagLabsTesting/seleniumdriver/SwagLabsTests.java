391a55543aaf386dbca56e8ad63a3811c66d2f09
nastiashpyt <nastiashpyt>
2022-11-10 15:25:54 +0000

    Initial commit

--- SwagLabsTesting/src/test/java/com/sparta/SwagLabsTesting/seleniumdriver/SwagLabsTests.java
+++ SwagLabsTesting/src/test/java/com/sparta/SwagLabsTesting/seleniumdriver/SwagLabsTests.java
@@ -8,19 +8,19 @@ import org.openqa.selenium.chrome.ChromeDriverLogLevel;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class SwagLabsTests {
-        private static WebDriver webDriver;
+    private static WebDriver webDriver;
+    @BeforeAll
+    public static void setupAll() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setLogLevel(ChromeDriverLogLevel.SEVERE);
+        webDriver = new ChromeDriver(chromeOptions);
+    }
 
-        @BeforeAll
-        public static void setupAll() {
-            System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-            ChromeOptions chromeOptions = new ChromeOptions();
-            chromeOptions.setLogLevel(ChromeDriverLogLevel.SEVERE);
-            webDriver = new ChromeDriver(chromeOptions);
-        }
     @BeforeEach
     public void setup() {
         webDriver.manage().deleteAllCookies();
         webDriver.get("https://www.saucedemo.com/");
     }
 
-    }
+}
