b377561df6be43d7b688d18d2db6def613b9d83f
nastiashpyt <nastiashpyt>
2022-11-11 17:30:47 +0000

    Changed Menu class (added WebDriverWait object as a field to use it at methods for waiting until links are clickable)

--- SwagLabsTesting/src/test/java/com/sparta/SwagLabsTesting/framework/pom/Menu.java
+++ SwagLabsTesting/src/test/java/com/sparta/SwagLabsTesting/framework/pom/Menu.java
@@ -2,70 +2,56 @@ package com.sparta.SwagLabsTesting.framework.pom;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 
 public class Menu {
 
-    private WebDriver webDriver;
-    private By crossButton = new By.ByClassName("bm-cross-button");
-    private By inventoryLink = new By.ByLinkText("All Items");
-    private By aboutLink = new By.ByLinkText("About");
-    private By logoutLink = new By.ByLinkText("Logout");
-    private By resetLink = new By.ByLinkText("Reset App State");
+    private final WebDriver webDriver;
+    WebDriverWait wait;
+
+    private final By crossButton = new By.ByClassName("bm-cross-button");
+    private final By inventoryLink = new By.ById("inventory_sidebar_link");
+    private final By aboutLink = new By.ById("about_sidebar_link");
+    private final By logoutLink = new By.ById("logout_sidebar_link");
+    private final By resetLink = new By.ById("reset_sidebar_link");
 
     public Menu(WebDriver webDriver) {
+        this.webDriver = webDriver;
+        this.wait = new WebDriverWait(webDriver, Duration.ofSeconds(3));
     }
 
-
-    public Menu xButton(){
-        webDriver.findElement(crossButton).click();
-        return new Menu(webDriver);
-
+    public void waitUntilElementClickable(By element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element));
     }
 
-    public InventoryPage goToInventory(){
+    public InventoryPage goToInventoryPage(){
+        waitUntilElementClickable(inventoryLink);
         webDriver.findElement(inventoryLink).click();
         return new InventoryPage(webDriver);
     }
 
-//    public void goToInventoryPage() {
-//        webDriver.get("https://www.saucedemo.com/inventory.html");
-//    }
-
     public void goToAboutPage(){
+        waitUntilElementClickable(aboutLink);
         webDriver.findElement(aboutLink).click();
     }
 
     public LoginPage goToLogout(){
+        waitUntilElementClickable(logoutLink);
         webDriver.findElement(logoutLink).click();
         return new LoginPage(webDriver);
     }
 
-
     public InventoryPage resetPage(){
+        waitUntilElementClickable(resetLink);
         webDriver.findElement(resetLink).click();
         return new InventoryPage(webDriver);
     }
 
-
-
-//    public AboutPage goToTheAboutPage(){
-//        webDriver.findElement(aboutLink).click();
-//        return new AboutPage(webDriver);
-//
-//    }
-
-
-
-//    public InventoryPage goToAboutPage(){
-//        webDriver.findElement(aboutLink).click();
-//        return new InventoryPage(webDriver);
-//
-//    }
-
     public void close(){
         webDriver.findElement(crossButton).click();
     }
-
-
 }
