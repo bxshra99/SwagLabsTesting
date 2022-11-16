ee5f99899b714e6b1c51570dbd8f586061771932
nastiashpyt <nastiashpyt>
2022-11-16 10:54:50 +0000

    Added helper methods

--- SwagLabsTesting/src/test/java/com/sparta/SwagLabsTesting/framework/pom/Cart.java
+++ SwagLabsTesting/src/test/java/com/sparta/SwagLabsTesting/framework/pom/Cart.java
@@ -1,5 +1,6 @@
 package com.sparta.SwagLabsTesting.framework.pom;
 
+import io.cucumber.java.sl.In;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -18,7 +19,10 @@ public class Cart {
     private final By removeButton = new By.ByCssSelector(".cart_button");
 //    private final By cartItem = new By.ByClassName("cart_item");
 
+    private final By continueButton = new By.ByName("continue-shopping");
     private final By checkoutLink = new By.ById("checkout");
+
+    private final By bage = new By.ByClassName("shopping_cart_badge");
 
     public List<WebElement> getCartItems() {
         return cartItems;
@@ -32,14 +36,12 @@ public class Cart {
         this.webDriver = webDriver;
         this.wait = new WebDriverWait(webDriver, Duration.ofSeconds(3));
         this.cartItems = webDriver.findElements(By.className("cart_item"));
-        //
     }
 
     public Menu openMenu() {
         webDriver.findElement(openMenuButton).click();
         return new Menu(webDriver);
     }
-
 
     public void clickRemoveButton(int index) {
         cartItems.get(index).findElement(removeButton).click();
@@ -50,11 +52,21 @@ public class Cart {
             cartItems.get(i).findElement(removeButton).click();
     }
 
+    public int getNumberOfItems() {
+        return Integer.parseInt(webDriver.findElement(bage).getText());
+    }
+
     public String getUrl() {
         return webDriver.getCurrentUrl();
     }
 
-    public CheckoutPage checkoutButton(){
+    public InventoryPage continueShopping() {
+        waitUntilElementClickable(continueButton);
+        webDriver.findElement(continueButton).click();
+        return new InventoryPage(webDriver);
+    }
+
+    public CheckoutPage goToCheckout(){
         waitUntilElementClickable(checkoutLink);
         webDriver.findElement(checkoutLink).click();
         return new CheckoutPage(webDriver);
